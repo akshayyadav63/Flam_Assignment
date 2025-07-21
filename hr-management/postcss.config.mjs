18e792af8f7e2c2147d1d484ec0184ae9b147ad2
akshayyadav63 <akshay.kumar_cs22@gla.ac.in>
2025-07-21 19:13:01 +0530

    clean ui

--- hr-management/postcss.config.mjs
+++ hr-management/postcss.config.mjs
@@ -1,6 +1,16 @@
+// tailwind.config.js
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
+ 
+   content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+ darkMode: 'class', // or 'media' depending on your preference
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
+
 export default config;
